2eda787596641841561b6471389081988b46b466
Tan Aik Chong <atan160@e.ntu.edu.sg>
2024-10-08 18:39:03 +0800

    updated logic, less indentations

--- HMSmain.java
+++ HMSmain.java
@@ -11,62 +11,58 @@ public class HMSmain {
         System.out.print("Are you a patient or staff? (Enter P for Patient, S for Staff): ");
         String userType = sc.nextLine().toUpperCase();
         boolean isPatient = userType.equals("P");
-
+        String hospitalID = null;
+        String password = null;
         while (!loggedIn) { 
             System.out.print("Hospital ID: ");
-            String hospitalID = sc.nextLine();
+            hospitalID = sc.nextLine();
 
             System.out.print("Password: ");
-            String password = sc.nextLine(); 
+            password = sc.nextLine(); 
 
             
             loggedIn = LoginManager.authenticateUser(hospitalID, password, isPatient);
-
-            if (loggedIn) {  
-                //temporarily only have patient. implement rest later.
-                Patient newPatient = new Patient(hospitalID, password, MedicalRecordsManager.loadRecords(hospitalID));
-                System.out.println("Login successful! Welcome to the system.");
-                newPatient.displayMenu();  
-                int choice = sc.nextInt();
-                sc.nextLine();
-                while(choice!=9){
-                    switch (choice){
-                        case 1: 
-                            newPatient.viewMedicalRecords();
-                            break;
-                        case 2:
-                            System.out.println("What is your new email address?");
-                            String newAdd = sc.nextLine();
-                            System.out.println("What is your new phone number?");
-                            String newNumber = sc.nextLine();
-                            newPatient.updateInfo(newAdd, newNumber);
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                        case 6:
-                            break;
-                        case 7:
-                            break;
-                        case 8:
-                            break;
-                        case 9:
-                            loggedIn = false;
-                            break;
-                    }
-                    System.out.print("Choose another option...");
-                    choice = sc.nextInt();
-                    sc.nextLine();
-                }   
+            if(loggedIn){
+                break;
             }
-            else {
-                System.out.println("Invalid credentials. Please try again.");
-            }
+            System.out.println("Invalid credentials. Please try again.");
         }
-
-        sc.close();
+    
+        Patient newPatient = new Patient(hospitalID, password, MedicalRecordsManager.loadRecords(hospitalID));
+        System.out.println("Login successful! Welcome to the system.");
+        newPatient.displayMenu();  
+        while(true){
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice){
+                case 1: 
+                    newPatient.viewMedicalRecords();
+                    break;
+                case 2:
+                    System.out.println("What is your new email address?");
+                    String newAdd = sc.nextLine();
+                    System.out.println("What is your new phone number?");
+                    String newNumber = sc.nextLine();
+                    newPatient.updateInfo(newAdd, newNumber);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    System.out.println("You have been logged out.");
+                    sc.close();
+                    System.exit(0);
+            }
+            System.out.print("Choose another option: ");
+        }
     }
 }
