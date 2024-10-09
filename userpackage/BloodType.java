66ce00db28dbd1f5b6f2d2f6437a51278dd3098b
Tan Aik Chong <atan160@e.ntu.edu.sg>
2024-10-09 13:23:50 +0800

    added toString method to convert from enum back to string

--- userpackage/BloodType.java
+++ userpackage/BloodType.java
@@ -25,4 +25,28 @@ public enum BloodType {
                 throw new IllegalArgumentException("Unknown blood type: " + bloodTypeStr);
         }
     }
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case A_POSITIVE:
+                return "A+";
+            case A_NEGATIVE:
+                return "A-";
+            case B_POSITIVE:
+                return "B+";
+            case B_NEGATIVE:
+                return "B-";
+            case AB_POSITIVE:
+                return "AB+";
+            case AB_NEGATIVE:
+                return "AB-";
+            case O_POSITIVE:
+                return "O+";
+            case O_NEGATIVE:
+                return "O-";
+            default:
+                throw new AssertionError("Unknown blood type: " + this);
+        }
+    }
 }
