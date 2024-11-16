67237fde78dc875fbeba5a05921d8c4aa2910fb2
Tan Aik Chong <atan160@e.ntu.edu.sg>
2024-11-16 21:32:55 +0800

    fix aor

---
+++ entities/PrescriptionStatus.java
@@ -0,0 +1,30 @@
+package entities;
+
+import java.util.zip.DeflaterInputStream;
+
+public enum PrescriptionStatus {
+    PENDING, DISPENSED;
+
+    public static PrescriptionStatus fromString(String prescriptionString) {
+        switch (prescriptionString.toLowerCase()) {
+            case "pending":
+                return PENDING;
+            case "dispensed":
+                return DISPENSED;
+            default:
+                throw new IllegalArgumentException("Unknown prescription: " + prescriptionString);
+        }
+    }
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case PENDING:
+                return "PENDING";
+            case DISPENSED:
+                return "DISPENSED";
+            default:
+                throw new AssertionError("Unknown prescription: " + this);
+        }
+    }
+}
